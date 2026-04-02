f1bfcb5c41f17cae2a26b5ec667452bff804889f
Aman Kumar Maurya <amankrmj@gmail.com>
2026-04-02 11:43:30 +0530

    Add configuration classes for ModelMapper and MongoDB auditing

---
+++ src/main/java/org/fitness/ai_service/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package org.fitness.ai_service.config;
+
+import org.fitness.ai_service.model.entity.Recommendation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertCallback;
+
+import java.util.UUID;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+    @Bean
+    public BeforeConvertCallback<Recommendation> activityBeforeConvertCallback() {
+        return (activity, collection) -> {
+            if (activity.getId() == null) {
+                activity.setId(UUID.randomUUID().toString());
+            }
+            return activity;
+        };
+    }
+}
