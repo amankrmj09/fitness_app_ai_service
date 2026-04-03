596424783b3073691b8148b1d6bf931a45d17ef1
Aman Kumar Maurya <amankrmj@gmail.com>
2026-04-03 09:56:03 +0530

    Add ActivityType enum for fitness activities

---
+++ src/main/java/org/fitness/ai_service/model/enums/ActivityType.java
@@ -0,0 +1,12 @@
+package org.fitness.ai_service.model.enums;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    HIKING,
+    YOGA,
+    GYM_WORKOUT,
+    OTHER
+}
